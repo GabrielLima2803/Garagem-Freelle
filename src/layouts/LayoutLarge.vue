f62f355b41a8fd11db72c5f017c3559d1bdbe3d4
vittorms <vitor.msalomao@gmail.com>
2024-07-24 16:53:12 -0300

    Feat - Criação dos Layouts

---
+++ src/layouts/LayoutLarge.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import LargeHeader from '@/components/templates/large/LargeHeader.vue';
+  import LargeFooter from '@/components/templates/large/LargeFooter.vue';
+</script>
+<template>
+  <div id="layout-large">
+    <header>
+      <large-header />
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <large-footer />
+    </footer>
+  </div>
+</template>
