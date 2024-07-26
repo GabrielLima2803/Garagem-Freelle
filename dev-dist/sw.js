9638498ddcad752cc3cb20b2f550e0fdf5ca7108
kvernert <kvernert47@gmail.com>
2024-07-26 14:04:14 -0300

    Inclusão de Dados

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r8tut2a1b9o"
+    "revision": "0.c0onddti5ro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
